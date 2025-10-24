c7a7923ff3542d5e065be45fdfa0c03305886997
Alexander Pugachev <alexander.pugachev@gmail.com>
2025-10-24 22:56:08 +0100

    wip luhn

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -3,5 +3,24 @@ pub fn is_valid(code: &str) -> bool {
     if code.len() < 2 {
         return false;
     }
-    todo!("bytes to digits?")
+    code
+        .chars()
+        .rev()
+        .map(|c| {
+            c.to_digit(10).unwrap()
+        })
+        .enumerate()
+        .map(|(i, d)| {
+            if i % 2 == 1 {
+                let x = d * 2;
+                if x > 9 {
+                    x - 9
+                } else {
+                    x
+                }
+            } else {
+                d
+            }
+        })
+        .sum::<u32>() % 10 == 0
 }
