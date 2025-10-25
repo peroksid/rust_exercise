ff5c80cffab9658ae90f3e17ad6d0b70dc7b80ed
Alexander Pugachev <alexander.pugachev@gmail.com>
2025-10-25 20:05:22 +0100

    baked luhn

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -1,26 +1,30 @@
-/// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    if code.len() < 2 {
-        return false;
-    }
-    code
+    let mut invalid_char = false;
+    let mut digit_count = 0;
+    let luhn = code
         .chars()
         .rev()
-        .map(|c| {
-            c.to_digit(10).unwrap()
+        .filter(|c| {
+            let is_digit = c.is_ascii_digit();
+            if !(is_digit || *c == ' ') {
+                invalid_char = true
+            };
+            if is_digit {
+                digit_count += 1;
+            }
+            is_digit
         })
+        .map(|c| c.to_digit(10).unwrap())
         .enumerate()
         .map(|(i, d)| {
             if i % 2 == 1 {
                 let x = d * 2;
-                if x > 9 {
-                    x - 9
-                } else {
-                    x
-                }
+                if x > 9 { x - 9 } else { x }
             } else {
                 d
             }
         })
-        .sum::<u32>() % 10 == 0
+        .sum::<u32>()
+        % 10;
+    (digit_count > 1) && !invalid_char && luhn == 0
 }
