610c04a1f9038462ce13910138c59f85ec3aff2d
Alexander Pugachev <alexander.pugachev@gmail.com>
2025-10-27 20:39:16 +0000

    allergies before mentor

---
+++ allergies/src/lib.rs
@@ -0,0 +1,42 @@
+pub struct Allergies(u32);
+
+#[repr(u8)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum Allergen {
+    Eggs = 1,
+    Peanuts = 2,
+    Shellfish = 4,
+    Strawberries = 8,
+    Tomatoes = 16,
+    Chocolate = 32,
+    Pollen = 64,
+    Cats = 128,
+}
+const ALLERGEN_VALUES: [Allergen; 8/* in nightly std::mem::variant_count */] = [
+            Allergen::Eggs,
+            Allergen::Peanuts,
+            Allergen::Shellfish,
+            Allergen::Strawberries,
+            Allergen::Tomatoes,
+            Allergen::Chocolate,
+            Allergen::Pollen,
+            Allergen::Cats,
+        ];
+
+impl Allergies {
+    pub fn new(score: u32) -> Self {
+        Self(score)
+    }
+
+    pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
+        self.0 & *allergen as u32 != 0
+    }
+
+    pub fn allergies(&self) -> Vec<Allergen> {
+        ALLERGEN_VALUES
+            .iter()
+            .filter(|x| self.is_allergic_to(x))
+            .copied()
+            .collect()
+    }
+}
