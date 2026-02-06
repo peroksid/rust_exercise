4c968d25fe721c24864820fae48d707c976940a6
Alexander Pugachev <alexander.pugachev@gmail.com>
2026-02-06 14:34:33 +0000

    space age: remove unnessary number suffix

--- space-age/src/lib.rs
+++ space-age/src/lib.rs
@@ -7,7 +7,8 @@ impl From<u64> for Duration {
     }
 }
 
-const REFERENCE_YEAR: f64 = 31_557_600_f64;
+const REFERENCE_YEAR: f64 = 31_557_600.0;
+
 pub trait Planet {
     const FACTOR: f64;
     fn years_during(d: &Duration) -> f64 {
@@ -16,11 +17,11 @@ pub trait Planet {
 }
 pub struct Mercury;
 impl Planet for Mercury {
-    const FACTOR: f64 = 0.2408467_f64;
+    const FACTOR: f64 = 0.2408467;
 }
 pub struct Venus;
 impl Planet for Venus {
-    const FACTOR: f64 = 0.61519726_f64;
+    const FACTOR: f64 = 0.61519726;
 }
 pub struct Earth;
 impl Planet for Earth {
@@ -28,21 +29,21 @@ impl Planet for Earth {
 }
 pub struct Mars;
 impl Planet for Mars {
-    const FACTOR: f64 = 1.8808158_f64;
+    const FACTOR: f64 = 1.8808158;
 }
 pub struct Jupiter;
 impl Planet for Jupiter {
-    const FACTOR: f64 = 11.862615f64;
+    const FACTOR: f64 = 11.862615;
 }
 pub struct Saturn;
 impl Planet for Saturn {
-    const FACTOR: f64 = 29.447498_f64;
+    const FACTOR: f64 = 29.447498;
 }
 pub struct Uranus;
 impl Planet for Uranus {
-    const FACTOR: f64 = 84.016846_f64;
+    const FACTOR: f64 = 84.016846;
 }
 pub struct Neptune;
 impl Planet for Neptune {
-    const FACTOR: f64 = 164.79132_f64;
+    const FACTOR: f64 = 164.79132;
 }
