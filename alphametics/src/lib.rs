7184f2d2755b06a266c6f4641e199e315c8a81a7
Alexander Pugachev <alexander.pugachev@gmail.com>
2026-02-17 20:34:43 +0000

    alphametics: wip, approach with leading zeroes impossible

--- alphametics/src/lib.rs
+++ alphametics/src/lib.rs
@@ -1,50 +1,61 @@
 use std::collections::HashMap;
-use std::collections::HashSet;
+
+const DIGITS: [char;10] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']; 
+const DIGITS_FOR_FIRST: [char; 9] = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+type DigitList<'static> = &'static [char];
+
+type LetterVariants = HashMap<char, DigitList>;
+
+type Variant = HashMap<char, char>;
+
+type VariantList = Vec<Variant>;
+
+
+struct Variants {
+    letter_variants: LetterVariants,
+    indexes: HashMap<char, u8>,
+    order: Vec<char>,
+}
+
+
+impl Variants {
+    fn new(letter_variants: LetterVariants) -> Self {
+        let mut indexes = HashMap::new();
+        for k in letter_variants.keys() {
+            indexes.insert(*k, 0);
+        }
+        Self {
+            letter_variants: letter_variants.clone(),
+            indexes: indexes,
+            order: letter_variants.keys().map(|&x| x).collect::<Vec<char>>()
+        }
+    }
+}
+
+impl Iterator for Variants {
+    type Item = Variant;
+    fn next(&mut self) -> Option<Self::Item> {
+        /*let mut m = HashMap::new();
+        let mut progressed = false;
+        let mut need_tick = 
+        for k in self.order.iter() {
+            if self.indexes[k] != self.letter_variants[k].len() - 1 {
+                self.indexes[k] += 1;
+                progressed = true;
+                break;
+            }
+
+        }
+        */
+        None
+    }
+
+}
 
 fn drop_char(c: char, digits: &[char]) -> Vec<char> {
     let mut r = digits.to_owned();
     r.retain(|&x| x != c);
     r
-}
-
-type Variant = HashMap<char, char>;
-type VariantList = Vec<Variant>;
-
-fn gen_rec_single(chars: &mut Vec<char>, digits: Vec<char>) -> VariantList {
-    let c = chars.pop().unwrap();
-    let mut result = Vec::new();
-    for d in digits {
-        let mut m = HashMap::new();
-        m.insert(c, d);
-        result.push(m);
-    }
-    result
-}
-
-fn gen_rec_multi(chars: &mut Vec<char>, digits: Vec<char>) -> VariantList {
-    let c = chars.pop().unwrap();
-    let mut result = Vec::new();
-    for d in &digits.clone() {
-        let mut m: Variant = HashMap::new();
-        m.insert(c, *d);
-        let mut next_chars = chars.to_owned();
-        for inside_dict in gen_rec(&mut next_chars, drop_char(*d, &digits)) {
-            for (k, v) in inside_dict {
-                m.insert(k, v);
-            }
-            result.push(m.to_owned());
-        }
-    }
-    chars.push(c);
-    result
-}
-
-fn gen_rec(chars: &mut Vec<char>, digits: Vec<char>) -> VariantList {
-    match chars.len() {
-        0 => Vec::new(),
-        1 => gen_rec_single(chars, digits),
-        _ => gen_rec_multi(chars, digits),
-    }
 }
 
 fn extract_expression(expr: String) -> Vec<String> {
@@ -76,31 +87,37 @@ fn calculate_expr_sum(dict: &Variant, expr_list: &[String]) -> Result<u64, Strin
     Ok(expr_list.iter().map(|x| translate_term(dict, x)).sum())
 }
 
-fn collect_unique_letters(input: &str) -> Vec<char> {
-    input
-        .chars()
-        .filter(|c| c.is_ascii_alphabetic())
-        .collect::<HashSet<_>>()
-        .into_iter()
-        .collect::<_>()
-}
-
 fn digify_dict(d: Variant) -> HashMap<char, u8> {
     d.iter()
         .map(|(&k, &v)| (k, v.to_digit(10).unwrap() as u8))
         .collect::<_>()
 }
 
-fn list_variants(input: &str) -> VariantList {
-    gen_rec(
-        &mut collect_unique_letters(input),
-        vec!['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
-    )
+fn collect_letters_variants(input: &str) -> LetterVariants {
+    let mut m = HashMap::new();
+    let mut next_is_first = true;
+    for c in input.chars() {
+        if c.is_ascii_alphabetic() {
+            if next_is_first {
+                m.insert(c, &DIGITS_FOR_FIRST);
+            } else {
+                if !m.contains_key(&c) {
+                    m.insert(c, &DIGITS);
+                }
+            }
+            next_is_first = false;
+        } else {
+            next_is_first = true;
+        }
+    }
+    m
 }
 
 pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
-    let (left_expr_list, right_expr_list) = get_exprs_from_input(input);
-    for variant in list_variants(input) {
+   let (left_expr_list, right_expr_list) = get_exprs_from_input(input);
+   let letter_variants = collect_letters_variants(input);
+   let variants = Variants::new(letter_variants);
+    for variant in variants {
         match calculate_expr_sum(&variant, &left_expr_list) {
             Ok(left) => match calculate_expr_sum(&variant, &right_expr_list) {
                 Ok(right) => {
