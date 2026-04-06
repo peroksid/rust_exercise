9805ec850455e6a6a031f530555b6dce7bc69b60
Alexander Pugachev <alexander.pugachev@gmail.com>
2026-04-06 20:25:37 +0100

    acronym: adressed feedback

--- acronym/src/lib.rs
+++ acronym/src/lib.rs
@@ -1,40 +1,31 @@
-#[derive(Copy, Clone)]
 enum State {
     Space,
     Word,
 }
 
+fn is_space(c: &char) -> bool {
+    !c.is_ascii_alphabetic()
+}
+
 pub fn abbreviate(phrase: &str) -> String {
     let mut state = State::Space;
-    let mut previous_char_lowercase: Option<bool> = None;
-    phrase
-        .chars()
-        .filter(|&c| match (state, c.is_ascii_alphabetic(), c) {
-            (State::Space, true, _) => {
-                state = State::Word;
-                previous_char_lowercase = Some(c.is_ascii_lowercase());
-                true
-            }
-            (State::Space, false, _) => false,
-            (State::Word, true, _) => match (c.is_ascii_lowercase(), previous_char_lowercase) {
-                (false, Some(true)) => {
-                    previous_char_lowercase = Some(c.is_ascii_lowercase());
-                    true
+    let mut result: Vec<char> = Vec::new();
+    for c in phrase.chars() {
+        match state {
+            State::Space => {
+                if !is_space(&c) {
+                    state = State::Word;
+                    result.push(c.to_ascii_uppercase());
                 }
-                (_, _) => {
-                    previous_char_lowercase = Some(c.is_ascii_lowercase());
-                    false
+            }
+            State::Word => {
+                if is_space(&c) {
+                    state = State::Space;
+                } else if c.is_ascii_uppercase() {
+                    result.push(c);
                 }
-            },
-            (State::Word, false, '\'') => {
-                previous_char_lowercase = None;
-                false
             }
-            (State::Word, false, _) => {
-                state = State::Space;
-                false
-            }
-        })
-        .map(|c| c.to_ascii_uppercase())
-        .collect()
+        }
+    }
+    result.into_iter().collect()
 }
