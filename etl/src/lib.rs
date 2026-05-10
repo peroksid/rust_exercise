d135ffc0e8f975805addebf2ced0c16bdd68d6f0
Alexander Pugachev <alexander.pugachev@gmail.com>
2026-05-11 00:01:50 +0100

    etl: removed moving

--- etl/src/lib.rs
+++ etl/src/lib.rs
@@ -2,6 +2,6 @@ use std::collections::BTreeMap;
 
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
     h.iter()
-        .flat_map(|(&cost, chars)| chars.iter().map(move |&ch| (ch.to_ascii_lowercase(), cost)))
+        .flat_map(|(cost, chars)| chars.iter().map(|ch| (ch.to_ascii_lowercase(), *cost)))
         .collect()
 }
